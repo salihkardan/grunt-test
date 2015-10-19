9c103df54190abe52c022e782ada7355ab794a1d
Salih Kardan <salihkardan@gmail.com>
2015-10-19 17:21:34 +0300

    jade support is added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 var pkgjson = require('./package.json');
 
 var config = {
-  pkg: pkgjson,
-  app: 'src',
-  dist: 'dist'
+	pkg: pkgjson,
+	app: 'src',
+	dist: 'dist'
 }
 
 var mybower = {
-  directory : 'src/components',
-  json : 'bower.json'
+	directory : 'src/components',
+	json : 'bower.json'
 }
 
 module.exports = function(grunt) {
@@ -17,73 +17,99 @@ module.exports = function(grunt) {
 		pkg: config.pkg,
 		// bower: grunt.file.readJSON('./.bowerrc'),
 		bower: mybower,
-    connect: {
-      server: {
-        options:{
-          port: 9001,
-          base: 'src'
-        }
-      }
-    },
-		copy: {
-			dist: {
-			 files: [{
-				 expand: true,
-				 cwd: '<%= config.app %>/components/font-awesome',
-				 src: 'css/font-awesome.min.css',
-				 dest: '<%= config.dist %>'
-			 },
-			 {
-				 expand: true,
-				 cwd: '<%= config.app %>/components/font-awesome',
-				 src: 'fonts/*',
-				 dest: '<%= config.dist %>'
-			 }]
+
+		connect: {
+			server: {
+				options:{
+					port: 9001,
+					base: 'src'
+				}
 			}
 		},
+
+		jade: {
+			compile: {
+				options: {
+					pretty: true,
+				},
+				files: {
+					'<%= config.app %>/views/build/index.html': '<%= config.app %>/views/jade/index.jade'
+				}
+			}
+		},
+
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= config.app %>/components/font-awesome',
+					src: 'css/font-awesome.min.css',
+					dest: '<%= config.dist %>'
+				},
+				{
+					expand: true,
+					cwd: '<%= config.app %>/components/font-awesome',
+					src: 'fonts/*',
+					dest: '<%= config.dist %>'
+				}]
+			}
+		},
+
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> lib - v<%= pkg.version %> -' +
-					'<%= grunt.template.today("yyyy-mm-dd") %> */'
+				'<%= grunt.template.today("yyyy-mm-dd") %> */'
 			},
 			dist: {
 				files: {
 					'<%= config.app %>/js/lib.min.js': [
-						'<%= bower.directory %>/jquery/dist/jquery.js',
-						'<%= bower.directory %>/underscore/underscore.js',
+					'<%= bower.directory %>/jquery/dist/jquery.js',
+					'<%= bower.directory %>/underscore/underscore.js',
 						// '<%= bower.directory %>/angular/angular.js',
-            '<%= bower.directory %>/noty/js/noty/packaged/jquery.noty.packaged.js',
-					]
+						'<%= bower.directory %>/noty/js/noty/packaged/jquery.noty.packaged.js',
+						]
+					}
 				}
-			}
-		},
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
-        }
-      }
-    },
-    watch: {
-      src: {
-        options: { livereload: true },
-        files: ['**/*.js', '**/*.html'],
-      },
-    },
-	});
-	require('load-grunt-tasks')(grunt);
-  grunt.registerTask('default', ['copy', 'uglify', 'connect', 'watch']);
-	grunt.registerTask('buildbower', [
-	  'bower_concat',
-	  'uglify:bower'
-	]);
-	grunt.registerTask('mybuild', [
-		'copy',
-		'uglify'
-	]);
-  grunt.registerTask('serve', [
-    'connect:server',
-    'watch'
-  ]);
+			},
+
+			jshint: {
+				files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+				options: {
+					globals: {
+						jQuery: true
+					}
+				}
+			},
+
+			watch: {
+				src: {
+					options: { livereload: true },
+					files: ['**/*.js', '**/*.jade'],
+				 	tasks: ['jade']
+				},
+			},
+		});
+
+		require('load-grunt-tasks')(grunt);
+
+		grunt.registerTask('default', ['copy', 'uglify', 'connect', 'jade', 'watch']);
+
+		grunt.registerTask('buildbower', [
+			'bower_concat',
+			'uglify:bower'
+			]);
+
+		grunt.registerTask('mybuild', [
+			'copy',
+			'uglify'
+			]);
+
+		grunt.registerTask('serve', [
+			'connect:server',
+			'watch'
+			]);
+
+		grunt.registerTask('build-jade', 'Convert Jade templates into html templates', [
+			'jade'
+			]);
 };
